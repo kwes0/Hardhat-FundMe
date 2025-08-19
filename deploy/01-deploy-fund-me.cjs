29765bde85b0095f89f711fdf418cce52c65db61
kwes0 <jacqueskamau@gmail.com>
2025-08-19 23:28:52 +0300

    Mocking and helper-hardhat-config

--- deploy/01-deploy-fund-me.cjs
+++ deploy/01-deploy-fund-me.cjs
@@ -1,25 +1,20 @@
-module.exports = async (hre) => {
-    //Pull values from hre
-    const {getNamedAccounts, deployments} = hre
-    // we can also pull the above values as arguements in the async function. See the example below setup
-
-    //get values from the getNamedAccounts() and deployments
-    const {deploy, log} = deployments
-    const {deployer} = await getNamedAccounts
-}
-
-
 /*
 Traditional deploying steps
     - import contract
     - main function
     - calling of main function
+
 With hardhat-deploy:
     - importing
     - a function to handle deployment which is exported as the default function for hardhat deploy to look for
     function deployFunc(hre){}
     module.exports.default = deployFunc
+*/
 
+const { getNamedAccounts, deployments, config } = require("hardhat");
+
+// Deploy Function Type 1
+/*
 Another way to write deploy function script - An anonymous async function
     module.exports = async({getNamedAccounts, deployments, getChainId, getUnnamedAccounts,})=>{
         const {deploy} = deployments; 
@@ -32,3 +27,24 @@ Another way to write deploy function script - An anonymous async function
         })
         }
 */
+
+//Deploy function type 2
+/*
+module.exports = async (hre) => {
+    //Pull out values from hre while running the function
+    const {getNamedAccounts, deployments} = hre
+    // we can also pull the above values as arguements in the async function. See the example below setup
+
+    //get values from the getNamedAccounts() and deployments
+    const {deploy, log} = deployments
+    const {deployer} = await getNamedAccounts
+}
+
+*/
+
+//Deploy function type 3
+module.exports = async ({getNamedAccounts, deployments}) => {
+    const {deploy, log} = deployments; //This two are being grabbed from deployments which is being grabbed from HRE
+    const {deployer} = await getNamedAccounts();
+    const chainId = network.config.chainId
+}
--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -5,4 +5,10 @@ require("hardhat-deploy")
 module.exports = {
     solidity: "0.8.19",
     defaultNetwork: "hardhat",
+    namedAccounts:{
+        deployer:{ // This is used to set a default deployer relatve to the deploy anonymous func
+            default: 0, //Sets the the account from the list of accounts provided.
+            31337: 1, //Choose the position of the chain
+        }
+    }
 }
